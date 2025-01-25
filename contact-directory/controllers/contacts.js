220f7d020236a6403070395111ca880e823f7f76
NicolasV99 <legolasnicolas99@gmail.com>
2025-01-25 14:07:11 -0500

    code updated for video test

--- contact-directory/controllers/contacts.js
+++ contact-directory/controllers/contacts.js
@@ -13,56 +13,88 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     //#swagger.tags=['Contacts']
-    const userId = new ObjetcId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId});
-    result.toArray().then((contacts) => {
-        res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(contacts[0]);        
-    });
+    try {
+        const userId = new ObjetcId(req.params.id);
+        const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: userId});
+        result.toArray().then((contacts) => {
+            res.setHeader('Content-Type', 'application/json');
+            res.status(200).json(contacts[0]);        
+        });
+    } catch (error) {
+        console.error(error.message); 
+        res.status(500).json({ message: 'Some error occurred while retrieving the contact' }); 
+    }
+    
 };
 
 const createContact = async (req, res) => {
     //#swagger.tags=['Contacts']
-    const contact = {
-        name: req.body.name,
-        phone: req.body.phone,
-        email: req.body.email,
-        address: req.body.address,
-    };
-    const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
-    if (response.acknowledged) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error ocurred while updating the user');
+    //#swagger.summary='Create a new contact'
+    //#swagger.responses[204] = {
+    //    description: 'Contact created successfully'
+    // }
+    //#swagger.responses[500] = {
+    //    description: 'Error occurred while creating the contact',
+    //    schema: { message: 'Some error occurred while creating the contact' }
+    // }
+    try {
+        const contact = {
+            name: req.body.name,
+            phone: req.body.phone,
+            email: req.body.email,
+            address: req.body.address,
+        };
+        const response = await mongodb.getDatabase().db().collection('contacts').insertOne(contact);
+        if (response.acknowledged) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error ocurred while updating the user');
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Some error occurred while creating the contact' });
     }
+    
 };
 
 const updateContact = async (req, res) => {
     //#swagger.tags=['Contacts']
-    const userId = new ObjetcId(req.params.id);
-    const contact = {
-        name: req.body.name,
-        phone: req.body.phone,
-        email: req.body.email,
-        address: req.body.address,
-    };
-    const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId}, contact);
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error ocurred while updating the user');
+    try {
+        const userId = new ObjetcId(req.params.id);
+        const contact = {
+            name: req.body.name,
+            phone: req.body.phone,
+            email: req.body.email,
+            address: req.body.address,
+        };
+        const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId}, contact);
+        if (response.modifiedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error ocurred while updating the user');
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Some error occurred while updating the contact' });
     }
+    
 };
 
 const deleteContact = async(req, res) => {
     //#swagger.tags=['Contacts']
-    const userId = new ObjetcId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId});
-    if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error ocurred while updating the user');
+    try {
+        const userId = new ObjetcId(req.params.id);
+        const response = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: userId});
+        if (response.deletedCount > 0) {
+            res.status(204).send();
+        } else {
+            res.status(500).json(response.error || 'Some error ocurred while updating the user');
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ message: 'Some error occurred while deleting the contact' });
     }
+    
 };
 
 
